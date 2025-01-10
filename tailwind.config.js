df8f8e493770b89f9c58ec6141246604b9c3e63e
muhammad fazle ferino <m.fazle2006@gmail.com>
2025-01-10 08:12:40 +0700

    art java

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        Headers: '#C1BAA1',
+        desk: '#ECEBDE',
+      }
+    },
+  },
+  plugins: [],
+}
